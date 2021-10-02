f8e265e90e36ca11f79782c55f120cea1b9b197f
Borys Yermokhin <borys.yermokhin@metro-markets.de>
2021-10-02 23:18:18 +0200

    added routes without implementation

--- src/Controller/AdvisorController.php
+++ src/Controller/AdvisorController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\DTO\CreateAdvisorDTO;
 use App\Entity\Advisor;
 use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,6 +34,19 @@ class AdvisorController extends AbstractController
         $this->entityManager->flush();
 
         return new JsonResponse($newAdvisor->toArray());
+    }
+
+    public function delete(string $id): JsonResponse
+    {
+        $advisor = $this->entityManager->find(Advisor::class, Uuid::fromString($id));
+        if (is_null($advisor)) {
+            return new JsonResponse(['error' => sprintf('Advisor with %s id was not found', $id)], Response::HTTP_NOT_FOUND);
+        }
+
+        $this->entityManager->remove($advisor);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['result' => sprintf('Advisor %s was deleted successfully', $id)]);
     }
 
     private function performValidation(CreateAdvisorDTO $advisorDTO): ?JsonResponse
