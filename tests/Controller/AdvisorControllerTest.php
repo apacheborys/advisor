19ecb3de387a034433e2da944e32d0d484f9c909
Borys Yermokhin <borys.yermokhin@metro-markets.de>
2021-10-04 21:43:26 +0200

    add few tests

--- tests/Controller/AdvisorControllerTest.php
+++ tests/Controller/AdvisorControllerTest.php
@@ -24,6 +24,60 @@ class AdvisorControllerTest extends WebTestCase
         self::assertSame(json_decode($expectedResponse, true), $resultAdvisor);
     }
 
+    /**
+     * @dataProvider createAdvisorProvider
+     * @covers \App\Controller\AdvisorController::update
+     */
+    public function testUpdateAdvisor(string $payload, int $expectedStatus, string $expectedResponse): void
+    {
+        $client = self::createClient();
+        $client->request('POST', '/api/v1/advisor', [], [], [], $payload);
+        $response = $client->getResponse();
+
+        self::assertSame($expectedStatus, $response->getStatusCode());
+
+        $decodedPayload = json_decode($payload, true);
+        $resultAdvisor = json_decode($response->getContent(), true);
+        $advisorId = $resultAdvisor['id'];
+        unset($resultAdvisor['id']);
+        self::assertSame(json_decode($expectedResponse, true), $resultAdvisor);
+
+        $decodedPayload['name'] = 'Advisor new name';
+        $client->request('PATCH', '/api/v1/advisor/' . $advisorId, [], [], [], json_encode($decodedPayload));
+        $response = $client->getResponse();
+
+        self::assertSame(200, $response->getStatusCode());
+        $resultAdvisor = json_decode($response->getContent(), true);
+        self::assertSame($resultAdvisor['name'], $decodedPayload['name']);
+    }
+
+    /**
+     * @dataProvider createAdvisorProvider
+     * @covers \App\Controller\AdvisorController::delete
+     */
+    public function testDeleteAndGetAdvisor(string $payload, int $expectedStatus, string $expectedResponse): void
+    {
+        $client = self::createClient();
+        $client->request('POST', '/api/v1/advisor', [], [], [], $payload);
+        $response = $client->getResponse();
+
+        self::assertSame($expectedStatus, $response->getStatusCode());
+
+        $resultAdvisor = json_decode($response->getContent(), true);
+        $advisorId = $resultAdvisor['id'];
+        unset($resultAdvisor['id']);
+        self::assertSame(json_decode($expectedResponse, true), $resultAdvisor);
+
+        $client->request('DELETE', '/api/v1/advisor/' . $advisorId);
+        $response = $client->getResponse();
+        self::assertSame(200, $response->getStatusCode());
+
+        $client->request('GET', '/api/v1/advisor/' . $advisorId);
+        $response = $client->getResponse();
+
+        self::assertSame(404, $response->getStatusCode());
+    }
+
     public function createAdvisorProvider(): iterable
     {
         yield 'success case' => [
